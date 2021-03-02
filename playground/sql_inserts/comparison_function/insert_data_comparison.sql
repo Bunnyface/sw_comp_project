3cddf38ac48e21b1ec6154a36706e66dad1fb2b5
aakio <jani.aakio@tuni.fi>
2021-03-02 14:13:18 +0200

    Add comparison function

---
+++ playground/sql_inserts/comparison_function/insert_data_comparison.sql
@@ -0,0 +1,9 @@
+INSERT INTO releases VALUES ('First', '0.1');
+INSERT INTO releases VALUES ('Second', '0.2');
+
+INSERT INTO componentTable VALUES ('Java', '1.2');
+INSERT INTO componentTable VALUES ('Scala', '3.2');
+
+INSERT INTO junctionTable VALUES ('First', 'Java');
+INSERT INTO junctionTable VALUES ('First', 'Scala');
+INSERT INTO junctionTable VALUES ('Second', 'Scala');
