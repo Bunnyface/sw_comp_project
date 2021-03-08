f1be042d80e9c879b77ea667967022adafd3f932
petrinieminen <petriolavinieminen@gmail.com>
2021-03-08 19:02:31 +0200

    Table create queries

---
+++ postgres/SW COMPMOD.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "Component" (
+  "id" int PRIMARY KEY,
+  "Name" varchar,
+  "URL" varchar,
+  "Version" varchar,
+  "License" varchar,
+  "Copyright" varchar
+);
+
+CREATE TABLE "Module" (
+  "id" int PRIMARY KEY,
+  "Name" varchar
+);
+
+CREATE TABLE "ModuleComponent" (
+  "Module_id" int,
+  "Component_id" int,
+  "UsageType" varchar,
+  "Date" Date,
+  PRIMARY KEY ("Module_id", "Component_id")
+);
+
+ALTER TABLE "ModuleComponent" ADD FOREIGN KEY ("Module_id") REFERENCES "Module" ("id");
+
+ALTER TABLE "ModuleComponent" ADD FOREIGN KEY ("Component_id") REFERENCES "Component" ("id");
+
