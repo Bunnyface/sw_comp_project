a3b46ff25a574f826d365df43dd628e376134066
Sanni <onkalosanni@gmail.com>
2021-03-31 16:20:26 +0300

    add data insertion

--- angular/src/app/data-input/data-input.component.ts
+++ angular/src/app/data-input/data-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../config.service';
 
 @Component({
   selector: 'app-data-input',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-input.component.css']
 })
 export class DataInputComponent implements OnInit {
+  
+  public file: File | null = null;
 
+  // Read the contents of the file and add data to database.
+  onFileSelected(event): void {
 
-  constructor() { }
+    this.file = event.target.files[0];
+    var data: string = '';
+    var jsonData;
+
+    if (this.file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        data = event.target.result as string;
+        console.log(data);
+        jsonData = JSON.parse(data)
+        this.configService.addRelease(jsonData).subscribe(res => console.log(res));
+      }
+      reader.readAsText(this.file);
+    }
+
+  }
+
+  constructor(private configService: ConfigService) { }
 
   ngOnInit(): void {
   }
