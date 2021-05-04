a05933f4fb62542f8327c1c17e2c1980ba0de168
Kaisa Sormunen <kaisa.sormunen@tuni.fi>
2021-05-04 17:48:19 +0300

    rebasing

--- scala/src/test/scala/com/example/playground/MainTest.scala
+++ scala/src/test/scala/com/example/playground/MainTest.scala
@@ -1,10 +1,15 @@
 package com.example.playground
 
 import io.finch._
+import org.scalatest._
 import org.scalatest.FunSuite
+import org.scalatest.mockito.MockitoSugar
+import org.mockito.Mockito._
 
-class MainTest extends FunSuite {
+class MainTest extends FunSuite with MockitoSugar{
+
   test("healthcheck") {
-    assert(Main.healthcheck(Input.get("/")).awaitValueUnsafe() == Some("OK"))
+    assert(Main.healthcheck(Input.get("/")).awaitValue() == Some(Right("OK")))
   }
+
 }
