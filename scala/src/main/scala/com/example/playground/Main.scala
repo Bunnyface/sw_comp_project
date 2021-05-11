88536b43f441370465b5f8d7ac856bdd68cef5ee
Denis Kole <denis.kole@tuni.fi>
2021-05-11 20:12:09 +0300

    Added delete endpoint

--- scala/src/main/scala/com/example/playground/Main.scala
+++ scala/src/main/scala/com/example/playground/Main.scala
@@ -138,6 +138,24 @@ object Main extends App {
     };
   }
 
+  def deleteWithId: Endpoint[IO, Json] = delete("delete" :: path[String] :: path[Int]) {
+    (table: String, id: Int) =>
+      val response = deleteFunctions.deleteById(table, id)
+      if (response != null)
+        Ok(response)
+      else 
+        BadRequest(new Exception("Element not found"))
+  }
+
+  def deleteWithName: Endpoint[IO, Json] = delete("delete" :: path[String] :: path[String]) {
+    (table: String, identifier: String) =>
+      val response = deleteFunctions.deleteByName(table, identifier)
+      if (response != null)
+        Ok(response)
+      else
+        BadRequest(new Exception("Element not found"))
+  }
+
   val policy: Cors.Policy = Cors.Policy(
     allowsOrigin = _ => Some("*"),
     allowsMethods= _ => Some(Seq("GET", "POST", "PUT")),
@@ -146,7 +164,9 @@ object Main extends App {
 
   def service: Service[Request, Response] = Bootstrap
     .serve[Text.Plain](healthcheck)
-    .serve[Application.Json](compare :+: insert :+: update :+: releases :+: releaseInfo :+: insertMany :+: insertModule :+: insertComponent :+: insertSubComponent :+: insertComponentToModule :+: insertSubToComponent :+: getEverything)
+    .serve[Application.Json](compare :+: insert :+: update :+: releases :+: releaseInfo :+: insertMany
+      :+: insertModule :+: insertComponent :+: insertSubComponent :+: insertComponentToModule 
+      :+: insertSubToComponent :+: getEverything :+: deleteWithId :+: deleteWithName)
     .toService
 
   val corsService: Service[Request, Response] = new Cors.HttpFilter(Cors.UnsafePermissivePolicy).andThen(service)
