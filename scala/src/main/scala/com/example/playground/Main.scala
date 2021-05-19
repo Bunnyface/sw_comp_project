a95a4d6e5ce97d17bb60dcef7bbbb9bba25a5f57
aakio <jani.aakio@tuni.fi>
2021-05-19 18:33:27 +0300

    Add basic error handling

--- scala/src/main/scala/com/example/playground/Main.scala
+++ scala/src/main/scala/com/example/playground/Main.scala
@@ -48,119 +48,253 @@ object Main extends App with LazyLogging {
 
   def releases: Endpoint[IO, Json] = post("releases") {
     logger.debug("Getting releases");
-    Ok(retrieveFunctions.queryNames());
+    try {
+      var retval = retrieveFunctions.queryNames()
+      Ok(retval)
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception("Problem querying releases"));
+      }
+    }
   }
 
   def releaseInfo: Endpoint[IO, Json] = post("releases" :: path[String]) { relName: String =>
-    var relInfo = retrieveFunctions.queryRelease(relName);
-    if (relInfo == null)
-      NotFound(new Exception("Release not found"));
-    else
-      Ok(relInfo);
+    logger.debug(s"Getting releaseinfo for: $relName");
+    try {
+      var relInfo = retrieveFunctions.queryRelease(relName);
+      if (relInfo == null)
+        NotFound(new Exception("Release not found"));
+      else
+        Ok(relInfo);
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Could not query release $relName"));
+      }
+    }
   }
 
   def components: Endpoint[IO, Json] = post("components") {
-    println("Getting components");
-    Ok(retrieveFunctions.queryComponents());
+    logger.debug("Getting components");
+    try {
+      Ok(retrieveFunctions.queryComponents());
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Could not query components"));
+      }
+    }
   }
 
   def getEverything: Endpoint[IO, Json] = post("moduleData") {
-    Ok(retrieveFunctions.retrieveEverything());
+    logger.debug("Fetching everything");
+    try {
+      Ok(retrieveFunctions.retrieveEverything());
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Could not query everything"));
+      }
+    }
   }
 
   def compare: Endpoint[IO, Json] = post("compare" :: jsonBody[CompareRequest]) { req: CompareRequest =>
-    val response = compareFunctions.compare(req.first, req.second);
-    if (response != null)
-      Ok(response);
-    else
-      NotFound(new Exception("One of the requested releases was not found"));
+    logger.debug("Fetching everything");
+    try {
+      val response = compareFunctions.compare(req.first, req.second);
+      if (response != null)
+        Ok(response);
+      else
+        NotFound(new Exception("One of the requested releases was not found"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Could not compare"));
+      }
+    }
   }
 
   def insert: Endpoint[IO, Json] = put("insert" :: path[String] :: jsonBody[InsertRequest]) { (table: String, req: InsertRequest) =>
-    val response = sendFunctions.queryInsert(table, req.columns, req.data);
-    if (response.length != 0)
-      Created(response.asJson);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting to $table");
+    try {
+      val response = sendFunctions.queryInsert(table, req.columns, req.data);
+      if (response.length != 0)
+        Created(response.asJson);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Could not insert to $table"));
+      }
+    }
   }
 
 
   def insertMany: Endpoint[IO, Json] = put("insertMany" :: jsonBody[Array[Json]]) { reqBody: Array[Json] =>
-    val response = sendFunctions.insertMany(reqBody);
-    if (response != null)
-      Ok(response);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting multiple");
+    try {
+      val response = sendFunctions.insertMany(reqBody);
+      if (response != null)
+        Ok(response);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to insert"));
+      }
+    }
   }
 
 
   def insertModule: Endpoint[IO, Json] = put("insertModule":: jsonBody[dbmodels.module]) { ( req: dbmodels.module) =>
-    val response = sendFunctions.insertModule(req);
-    if (response.length != 0)
-      Created(response.asJson);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting module");
+    try {
+      val response = sendFunctions.insertModule(req);
+      if (response.length != 0)
+        Created(response.asJson);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to insert module"));
+      }
+    }
   }
 
   def insertComponent: Endpoint[IO, Json] = put("insertComponent":: jsonBody[dbmodels.component]) { ( req: dbmodels.component) =>
-    val response = sendFunctions.insertComponent(req);
-    if (response.length != 0)
-      Created(response.asJson);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting component");
+    try {
+      val response = sendFunctions.insertComponent(req);
+      if (response.length != 0)
+        Created(response.asJson);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to insert component"));
+      }
+    }
   }
 
   def insertSubComponent: Endpoint[IO, Json] = put("insertSubComponent":: jsonBody[dbmodels.subComponent]) { ( req: dbmodels.subComponent) =>
-    val response = sendFunctions.insertSubComponent(req);
-    if (response.length != 0)
-      Created(response.asJson);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting subcomponent");
+    try {
+      val response = sendFunctions.insertSubComponent(req);
+      if (response.length != 0)
+        Created(response.asJson);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to insert subcomponent"));
+      }
+    }
   }
 
   def insertComponentToModule: Endpoint[IO, Json] = put("insertComponentToModule":: jsonBody[dbmodels.componentToModule]) { ( req: dbmodels.componentToModule) =>
-    val response = sendFunctions.insertComponentToModel(req);
-    if (response.length != 0)
-      Created(response.asJson);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting component to module");
+    try {
+      val response = sendFunctions.insertComponentToModel(req);
+      if (response.length != 0)
+        Created(response.asJson);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to insert component to module"));
+      }
+    }
   }
 
   def insertSubToComponent: Endpoint[IO, Json] = put("insertSubToComponent":: jsonBody[dbmodels.junction]) { ( req: dbmodels.junction) =>
-    val response = sendFunctions.insertSubToComp(req);
-    if (response.length != 0)
-      Created(response.asJson);
-    else
-      BadRequest(new Exception("Table not found or data is corrupted"));
+    logger.debug(s"Inserting subcomponent to component");
+    try {
+      val response = sendFunctions.insertSubToComp(req);
+      if (response.length != 0)
+        Created(response.asJson);
+      else
+        BadRequest(new Exception("Table not found or data is corrupted"));
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to insert subcomponent to component"));
+      }
+    }
   }
 
   def update: Endpoint[IO, Json] = post("update" :: jsonBody[UpdateRequest]) { req: UpdateRequest =>
-    val response =
-      sendFunctions.queryUpdate(req.table, req.newValCol, req.newVal, req.condCol, req.condVal);
-    if (response != null) {
-      Created(response)
-    };
-    else {
-      BadRequest(new Exception("Release not found"));
-    };
+    logger.debug(s"Updating a row");
+    try {
+      val response =
+        sendFunctions.queryUpdate(req.table, req.newValCol, req.newVal, req.condCol, req.condVal);
+      if (response != null) {
+        Created(response)
+      };
+      else {
+        BadRequest(new Exception("Release not found"));
+      };
+    }
+    catch {
+      case t: Throwable => {
+        logger.error(t.toString);
+        InternalServerError(new Exception(s"Failed to update"));
+      }
+    }
   }
 
   def deleteWithId: Endpoint[IO, Json] = delete("delete" :: path[String] :: path[Int]) {
     (table: String, id: Int) =>
-      val response = deleteFunctions.deleteById(table, id)
-      if (response != null)
-        Ok(response)
-      else 
-        BadRequest(new Exception("Element not found"))
+      logger.debug(s"Deleting with id: $id");
+      try {
+        val response = deleteFunctions.deleteById(table, id)
+        if (response != null)
+          Ok(response)
+        else
+          BadRequest(new Exception("Element not found"))
+      }
+      catch {
+        case t: Throwable => {
+          logger.error(t.toString);
+          InternalServerError(new Exception(s"Failed to delete id: $id"));
+        }
+      }
   }
 
   def deleteWithName: Endpoint[IO, Json] = delete("delete" :: path[String] :: path[String]) {
     (table: String, identifier: String) =>
-      val response = deleteFunctions.deleteByName(table, identifier)
-      if (response != null)
-        Ok(response)
-      else
-        BadRequest(new Exception("Element not found"))
+      logger.debug(s"Deleting with name: $identifier");
+      try {
+        val response = deleteFunctions.deleteByName(table, identifier)
+        if (response != null)
+          Ok(response)
+        else
+          BadRequest(new Exception("Element not found"))
+      }
+      catch {
+        case t: Throwable => {
+          logger.error(t.toString);
+          InternalServerError(new Exception(s"Failed to delete name: $identifier"));
+        }
+      }
   }
 
   val policy: Cors.Policy = Cors.Policy(
